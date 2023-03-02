10f2744d4046e3c9bc87a88b0a22c2533ba3d546
Hanni <ngynngchn@gmail.com>
2023-03-02 14:53:53 +0100

    Added new data

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,3 +1,7 @@
-let types = [{ type: "Bug", color: "green" }];
+let types = [
+	{ type: "Bug", color: "green" },
+	{ type: "Bug", color: "green" },
+	{ type: "Bug", color: "green" },
+];
 
 /* die restlichen types und dazugehörigen color (als HEX-Code) */
