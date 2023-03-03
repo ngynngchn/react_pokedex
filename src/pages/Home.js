b54eba2e5220100cf8d6750d2313d64fd6f0bc26
Hanni <ngynngchn@gmail.com>
2023-03-03 12:08:07 +0100

    made changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,25 @@
 import Navigation from "../components/navigation/Navigation";
 import PokemonList from "../components/pokemonlist/PokemonList";
 
+// Library import
+import { useState } from "react";
+
+// import von Pokeminfilter hier, da es von überall zugreifbar sein sollte
+import PokemonFilter from "../components/pokemonfilter/PokemonFilter";
+
 const Home = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+	// funktion die dann an die PokemonList weitergegeben werden muss
+	function searchPokemon(event) {
+		setSearchTerm(event.target.value);
+	}
 	return (
 		<main className="Home">
-			<Navigation home />
-			<PokemonList />
+			<PokemonFilter />
+			<Navigation home handleChange={searchPokemon} />
+			{/* searchTerm um später dann die Pokemonliste nach dem Pokemon zu filtern
+			 */}{" "}
+			<PokemonList searchTerm={searchTerm} />
 		</main>
 	);
 };
