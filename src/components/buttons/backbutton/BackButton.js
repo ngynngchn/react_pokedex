5a6991844f5f07f14718bd24ba8d30b61cac7d06
Hanni <ngynngchn@gmail.com>
2023-03-03 12:02:34 +0100

    Added back button image

--- src/components/buttons/backbutton/BackButton.js
+++ src/components/buttons/backbutton/BackButton.js
@@ -1,8 +1,25 @@
 // CSS import
 import "./BackButton.css";
 
-const BackButton = () => {
-	return <button>Back</button>;
+// Image import
+import backIcon from "../../../images/back.svg";
+
+// Library Import
+import { useNavigate } from "react-router-dom";
+
+const BackButton = (props) => {
+	let navigate = useNavigate();
+
+	return (
+		<input
+			type="image"
+			src={backIcon}
+			alt="back"
+			onClick={() => {
+				navigate(-1);
+			}}
+		/>
+	);
 };
 
 export default BackButton;
