de817caaf21342dfec30c0e3ced663b0a98b659f
karinlee <karinlee92@gmail.com>
2023-03-03 15:08:37 +0100

    added types

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,3 +1,6 @@
+/* //file import
+import { types } from "../data/data.js"; */
+
 //component import
 import Types from "../type/Types";
 
@@ -10,13 +13,25 @@ const Details = (props) => {
 	let id = props.pokemon.id.toString();
 	id = id.padStart(3,0);
 
+	/* Der Anfangsbuchstabe vom Pokemon soll groß geschrieben sein */
+	let pokemonName = (props.pokemon.name).charAt(0).toUpperCase()+(props.pokemon.name).slice(1);
+
+	let pokemonImg = props.pokemon.sprites.other.dream_world.front_default;
+
 	return (
-		<article id="pokemonDetail">
+		<article className="pokemonDetail">
 			<div className="imgBackground"></div>
-				<img src={props.pokemon.sprites.other.dream_world.front_default} alt={props.pokemon.name} />
-			{/* Der Pokedex Eintrag wird hier angezeigt und der erste Buchstabe vom Namen soll großgeschrieben sein */}
-			<h1>#{id} {(props.pokemon.name).charAt(0).toUpperCase()+(props.pokemon.name).slice(1)}</h1>
-			<Types />
+				<img src={pokemonImg} alt={pokemonName} />
+			<h1>#{id} {pokemonName}</h1>
+			<article className="pokemonType">
+				{/* Hier werden die PokemonTypen(array) an die Komponente Type übergeben */}
+				{props.pokemon.types.map((type,index)=>{
+					return <Types
+					key = {index}
+					typeName = {type.type.name}
+					/>
+				})}
+			</article>
 			<h3>Attack & Movements</h3>
 		</article>
 	);
--- src/components/type/Types.js
+++ src/components/type/Types.js
@@ -1,7 +1,20 @@
+//file import
+import {types} from "../data/data.js";
+
 // CSS import
 import "./Types.css";
-const Types = () => {
-	return <h2>Types</h2>;
+
+const Types = (props) => {
+	let typeName = props.typeName.toUpperCase();
+	let color = types.find((type)=>{
+		return type.type === typeName
+	})
+	
+	console.log(color)
+
+	return (
+		<h4 style={{backgroundColor: color.color }}>{typeName}</h4>
+	);
 };
 
 export default Types;
